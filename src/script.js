22eafaf73adc7700b27efe3363f73d009dac9df3
Coderofcoders70 <Lakshayas918@gmail.com>
2025-08-23 17:42:00 +0530

    Added few changes

--- src/script.js
+++ src/script.js
@@ -2,5 +2,33 @@ const menuBtn = document.getElementById('menu-btn');
 const menu = document.getElementById('mobile-menu');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+    menu.classList.toggle('max-h-0');
+    menu.classList.toggle('opacity-0');
+    menu.classList.toggle('pointer-events-none');
+    menu.classList.toggle('max-h-96');
+    menu.classList.toggle('opacity-100');
+
+    menu.classList.toggle('bg-[#F39C6B]');    
+});
+
+
+const searchIcon = document.getElementById('search-bar');
+const searchInput = document.getElementById('search-input');
+const searchPopup = document.getElementById('search-popup');
+const backdrop = document.getElementById('backdrop');
+
+searchIcon.addEventListener("click", () => {
+    backdrop.classList.remove("hidden");
+    backdrop.classList.add("opacity-100");
+    searchPopup.classList.remove("w-0", "opacity-0");
+    searchPopup.classList.add("w-full", "opacity-100");
+
+    setTimeout(() => searchInput.focus(), 300);
+});
+
+backdrop.addEventListener("click", () => {
+    backdrop.classList.add("hidden");
+    backdrop.classList.remove("opacity-100");
+    searchPopup.classList.add("w-0", "opacity-0");
+    searchPopup.classList.remove("w-full", "opacity-100");
 });
